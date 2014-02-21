2eeef43940b74e1501cd47cce9b9b9013f9b4871
Carl Meyer <carl@oddbird.net>
2014-02-20 19:20:17 -0700

    Add support for emphasizing lines in a code block.

--- output/js/oddbird.js
+++ output/js/oddbird.js
@@ -1,11 +1,29 @@
 (function ( document, window ) {
-    'use strict';
+  'use strict';
 
-    // LIs in top-level UL/OL (i.e. not notes) in data-reveal:1 step are innerSteps
-    [].slice.call(document.querySelectorAll(
-        '.step[data-reveal="1"] > ul > li, .step[data-reveal="1"] > ol > li'
-    )).forEach( function (elem) {
-        elem.classList.add('innerStep');
-    });
+  // LIs in top-level UL/OL (i.e. not notes) in data-reveal:1 step are innerSteps
+  [].slice.call(document.querySelectorAll(
+    '.step[data-reveal="1"] > ul > li, .step[data-reveal="1"] > ol > li'
+  )).forEach( function (elem) {
+    elem.classList.add('innerStep');
+  });
 
 })(document, window);
+
+
+$(function () {
+  $('.step').each(function () {
+    var slide = $(this);
+    var emph = slide.attr('data-emphasize-lines');
+    var linenos = slide.find('.code .ln');
+    if (emph) {
+      emph.split(',').forEach(function (num) {
+        // pad single digit with spaces to avoid matching within a double-digit
+        // line number
+        if (num.length === 1) { num = ' ' + num + ' '; }
+        linenos.filter(':contains(' + num + ')').addClass('emphasized');
+      });
+    }
+    linenos.html('&nbsp;');
+  });
+});
